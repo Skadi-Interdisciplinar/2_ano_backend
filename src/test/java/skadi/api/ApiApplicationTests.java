5b05837b2b358d2a8025ac2f1f6586f6c384215d
Enzo-Nasci <enzoeloa09@gmail.com>
2026-08-21 17:41:55 -0300

    initial commit

---
+++ src/test/java/skadi/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package skadi.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
